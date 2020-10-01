814a8d71435d97c00e9ffdd286866ec13c28fd61
nguyenthanh1995 <Thanhnguyennguyen1995@gmail.com>
2020-10-01 14:21:27 +0700

    .

---
+++ src/pages/404.vue
@@ -0,0 +1,40 @@
+<template>
+   <div class="error-404">
+      <img :src="require('@/assets/not.found.png')">
+      <p> Sorry, The Page Does Not Exists</p>
+      <p class="go-home"> Go To Home </p>
+   </div>
+</template>
+<style lang="scss" scoped>
+   .error-404 {
+      align-items: center;
+      display: flex;
+      flex-direction: column;
+      height: (100vh - 13.867vw);
+      justify-content: center;
+
+      img {
+         margin-bottom: 13.3333vw;
+         width: 80%;
+      }
+
+      p {
+         @include distance0;
+         text-align: center;
+         color: rgb(0, 142, 255);
+         font-size: 5.33333vw;
+      }
+
+      .go-home {
+         border-radius: 1.33333vw;
+         color: $white;
+         margin-top: 5.33333vW;
+         padding: 2.66667vw 5.33333vw;
+      }
+   }
+</style>
+<script>
+   export default {
+
+   }
+</script>
